1edeced9b7eb1b39900df7aefaae17cfd9433f6a
Christian Duerr <contact@christianduerr.com>
2018-05-27 01:07:32 +0200

    Properly redirect stderr in `start-server.sh`

--- start-server.sh
+++ start-server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Simple helper for starting the actix server
-cargo build --release 2&>> /tmp/alacritty-perf.log
-nohup cargo run --release 2&>> /tmp/alacritty-perf.log &
+cargo build --release &>> /tmp/alacritty-perf.log
+nohup cargo run --release &>> /tmp/alacritty-perf.log &
