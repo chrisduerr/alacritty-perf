7a3b0343fc85fed767f7316eaa9709320316352e
Christian Duerr <contact@christianduerr.com>
2018-10-10 22:55:44 +0200

    Remove double slash from results dir

--- src/main.rs
+++ src/main.rs
@@ -37,7 +37,7 @@ use walkdir::{DirEntry, WalkDir};
 static PUB_KEY: &'static [u8] = b"-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvtjdLkS+FP+0fPC09j25\ny/PiuYDDivIT86COVedvlElk99BBYTrqNaJybxjXbIZ1Q6xFNhOY+iTcBr4E1zJu\ntizF3Xi0V9tOuP/M8Wn4Y/1lCWbQKlWrNQuqNBmhovF4K3mDCYswVbpgTmp+JQYu\nBm9QMdieZMNry5s6aiMA9aSjDlNyedvSENYo18F+NYg1J0C0JiPYTxheCb4optr1\n5xNzFKhAkuGs4XTOA5C7Q06GCKtDNf44s/CVE30KODUxBi0MCKaxiXw/yy55zxX2\n/YdGphIyQiA5iO1986ZmZCLLW8udz9uhW5jUr3Jlp9LbmphAC61bVSf4ou2YsJaN\n0QIDAQAB\n-----END PUBLIC KEY-----";
 
 // Directory with all the benchmarks
-static RESULTS_DIR: &'static str = "./results/";
+static RESULTS_DIR: &'static str = "./results";
 
 #[derive(Deserialize)]
 struct Payload {
